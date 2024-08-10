ed9bb75833bfdf850297c64c29d15f9feeb23fd1
Mario <6395323+mario-huang@users.noreply.github.com>
2024-08-10 21:38:44 +0800

    add  task_to_id

--- download_pretrained_weights.py
+++ download_pretrained_weights.py
@@ -6,8 +6,25 @@ if __name__ == "__main__":
     """
     Download all pretrained weights
     """
-    for task_id in range(1, 1000):
-        try:
+    task_to_id = {
+        "total": [291, 292, 293, 294, 295, 298],
+        "total_fast": [297, 298],
+        "total_mr": [730, 731],
+        "total_fast_mr": [732, 733],
+        "lung_vessels": [258],
+        "cerebral_bleed": [150],
+        "hip_implant": [260],
+        "coronary_arteries": [503],
+        "pleural_pericard_effusion": [315],
+        "body": [299],
+        "body_fast": [300],
+        "head_glands_cavities": [775],
+        "headneck_bones_vessels": [776],
+        "head_muscles": [777],
+        "headneck_muscles": [778, 779],
+        "liver_vessels": [8],
+    }
+
+    for task in task_to_id:
+        for task_id in task_to_id[task]:
             download_pretrained_weights(task_id)
-        except ValueError:
-            continue
