9dc4fbc0b8b9ba9b4ad933664e762fbbd7f5d8a2
Mario <6395323+mario-huang@users.noreply.github.com>
2024-11-16 16:27:48 +0800

    add host

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ import shutil
 
 INPUTS_DIRECTORY = "inputs"
 OUTPUTS_DIRECTORY = "outputs"
-for directory in [INPUTS_DIRECTORY, OUTPUTS_DIRECTORY]:
-    if os.path.exists(directory):
-        shutil.rmtree(directory)
-    os.makedirs(directory)
 
 if __name__ == "__main__":
-    uvicorn.run("api:app", port=8000, log_level="info", works=os.cpu_count())
+    for directory in [INPUTS_DIRECTORY, OUTPUTS_DIRECTORY]:
+        if os.path.exists(directory):
+            shutil.rmtree(directory)
+        os.makedirs(directory)
+
+    uvicorn.run("api:app", host="0.0.0.0", port=8000, log_level="info", workers=os.cpu_count())
