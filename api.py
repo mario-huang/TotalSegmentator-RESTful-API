b7f65aed34a722f5496123da9aa8df7a1633380f
Mario <6395323+mario-huang@users.noreply.github.com>
2024-12-05 09:49:59 +0800

    delete segment_file

--- api.py
+++ api.py
@@ -6,13 +6,7 @@ import signal
 import time
 import os
 from urllib.parse import urlparse
-from fastapi import (
-    Depends,
-    FastAPI,
-    File,
-    Form,
-    UploadFile,
-)
+from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from main import INPUTS_DIRECTORY, OUTPUTS_DIRECTORY
 from totalsegmentator.python_api import totalsegmentator
@@ -36,86 +30,6 @@ def is_wsl():
 IS_WSL = is_wsl()
 
 app = FastAPI()
-
-
-@dataclass
-class FileRequest:
-    ml: bool = Form(True, description="Save one multilabel image for all classes")
-    nr_thr_resamp: int = Form(1, description="Nr of threads for resampling")
-    nr_thr_saving: int = Form(6, description="Nr of threads for saving segmentations")
-    fast: bool = Form(False, description="Run faster lower resolution model (3mm)")
-    nora_tag: str = Form(
-        "None", description="tag in nora as mask. Pass nora project id as argument."
-    )
-    preview: bool = Form(False, description="Generate a png preview of segmentation")
-    task: str = Form(
-        "total",
-        description="""
-        Select which model to use. This determines what is predicted.
-        Choices: 
-        "total", "body", "lung_vessels", "cerebral_bleed", "hip_implant", "coronary_arteries", "pleural_pericard_effusion", "test", "appendicular_bones", "tissue_types", "heartchambers_highres", "face", "vertebrae_body", "total_mr", "tissue_types_mr", "face_mr", "head_glands_cavities", "head_muscles", "headneck_bones_vessels", "headneck_muscles", "brain_structures", "liver_vessels"
-        """,
-    )
-    roi_subset: str = Form(
-        None,
-        description="Define a subset of classes to save (space separated list of class names). If running 1.5mm model, will only run the appropriate models for these rois.",
-    )
-    statistics: bool = Form(
-        False,
-        description="Calc volume (in mm3) and mean intensity. Results will be in statistics.json",
-    )
-    radiomics: bool = Form(
-        False,
-        description="Calc radiomics features. Requires pyradiomics. Results will be in statistics_radiomics.json",
-    )
-    crop_path: str = Form(
-        None,
-        description="Custom path to masks used for cropping. If not set will use output directory.",
-    )
-    body_seg: bool = Form(
-        False,
-        description="Do initial rough body segmentation and crop image to body region",
-    )
-    force_split: bool = Form(
-        False,
-        description="Process image in 3 chunks for less memory consumption. (do not use on small images)",
-    )
-    output_type: str = Form(
-        "nifti",
-        description="Select if segmentations shall be saved as Nifti or as Dicom RT Struct image. Choices: nifti, dicom",
-    )
-    quiet: bool = Form(False, description="Print no intermediate outputs")
-    verbose: bool = Form(False, description="Show more intermediate output")
-    test: int = Form(0, description="Only needed for unittesting. Choices: 0, 1, 3")
-    skip_saving: bool = Form(
-        False,
-        description="Skip saving of segmentations for faster runtime if you are only interested in statistics.",
-    )
-    device: str = Form(
-        "gpu", description="Device to run on (default: gpu). Choices: gpu, cpu, mps"
-    )
-    license_number: str = Form(
-        None,
-        description="Set license number. Needed for some tasks. Only needed once, then stored in config file.",
-    )
-    statistics_exclude_masks_at_border: bool = Form(
-        True,
-        description="Normally statistics are only calculated for ROIs which are not cut off by the beginning or end of image. Use this option to calc anyways.",
-    )
-    no_derived_masks: bool = Form(
-        False, description="Do not create derived masks (e.g. skin from body mask)."
-    )
-    v1_order: bool = Form(
-        False,
-        description="In multilabel file order classes as in v1. New v2 classes will be removed.",
-    )
-    fastest: bool = Form(
-        False, description="Run even faster lower resolution model (6mm)"
-    )
-    roi_subset_robust: str = Form(
-        None,
-        description="Like roi_subset but uses a slower but more robust model to find the rois.",
-    )
 
 
 class DataRequest(BaseModel):
@@ -202,27 +116,14 @@ class UrlRequest(BaseModel):
     data: DataRequest
 
 
-@app.post("/segment_file")
-async def segment_file(
-    input: UploadFile = File(
-        ..., description="CT nifti image or folder of dicom slices"
-    ),
-    body: FileRequest = Depends(),
-):
-    print("body: " + str(body))
-    return await add_timeout(input, asdict(body))
-
-
 @app.post("/segment_url")
 async def segment_url(body: UrlRequest):
     print("body: " + str(body))
-    return await add_timeout(body.input, asdict(body.data))
-
-
-async def add_timeout(input: str | UploadFile, body: dict[str, Any]):
     try:
         # sometimes Totalsegmentator hangs, so we need a timeout
-        return await asyncio.wait_for(process_segment(input, body), timeout=10 * 60)
+        return await asyncio.wait_for(
+            process_segment(body.input, body.data.model_dump()), timeout=10 * 60
+        )
     except asyncio.TimeoutError:
         print("Segmentation processing timed out.")
         terminate_process()
@@ -232,23 +133,16 @@ async def add_timeout(input: str | UploadFile, body: dict[str, Any]):
             terminate_process()
 
 
-async def process_segment(input: str | UploadFile, body: dict[str, Any]):
+async def process_segment(input: str, data: dict[str, Any]):
     timestamp_ms = time.time_ns() // 1000000
-    if isinstance(input, str):
-        filename_with_extension = os.path.basename(urlparse(input).path)
-        filename = os.path.splitext(filename_with_extension)[0]
-    else:
-        filename = input.filename
+    filename_with_extension = os.path.basename(urlparse(input).path)
+    filename = os.path.splitext(filename_with_extension)[0]
     input_path = os.path.join(INPUTS_DIRECTORY, str(timestamp_ms) + "-" + filename)
-
-    if isinstance(input, str):
-        await download_file(input, input_path)
-    else:
-        await save_file(input, input_path)
+    await download_file(input, input_path)
     try:
         output_path = os.path.join(OUTPUTS_DIRECTORY, str(timestamp_ms) + ".nii.gz")
         input_img = nib.load(input_path)
-        output_img = totalsegmentator(input_img, None, **body)
+        output_img = totalsegmentator(input_img, None, **data)
         nib.save(output_img, output_path)
         print("Yes! Totalsegmentator Finished!")
     except Exception as e:
